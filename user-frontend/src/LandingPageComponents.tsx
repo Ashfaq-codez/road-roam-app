fee6b37adfcaefbcf599244b31ad2ddb04211f2c
Ashfaq <ashrahman777@gmail.com>
2025-11-14 19:46:00 +0530

    fix: added back button to destinations menu redirects

--- user-frontend/src/LandingPageComponents.tsx
+++ user-frontend/src/LandingPageComponents.tsx
@@ -1,6 +1,6 @@
 // user-frontend/src/LandingPageComponents.tsx
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 /**
  * useHashNavigation
